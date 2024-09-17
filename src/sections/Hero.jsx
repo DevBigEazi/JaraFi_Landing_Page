53512e3a787ed36c95ae765d8c4fcee7ed6f247c
devbigeazi <adesholatajudeen1@gmail.com>
2024-09-17 15:08:01 +0100

    Implemented nav designs and responsiveness..

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,5 @@
 const Hero = () => {
-  return (
-    <section className="min-h-screen flex justify-center items-center">
-      Hero
-    </section>
-  );
+  return <section className="min-h-screen bg-linen pt-28 max-sm:pt-16">Hero</section>;
 };
 
 export default Hero;

7713099ca77fb6d6af97525d8afae5fe91165913
devbigeazi <adesholatajudeen1@gmail.com>
2024-09-19 11:44:16 +0100

    ...

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,7 @@
 const Hero = () => {
-  return <section className="min-h-screen bg-linen pt-28 max-sm:pt-16">Hero</section>;
+  return (
+    <section className="min-h-screen bg-linen pt-28 max-sm:pt-16">Hero</section>
+  );
 };
 
 export default Hero;

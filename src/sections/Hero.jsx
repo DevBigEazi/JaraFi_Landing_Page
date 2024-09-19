7dd37647789ce6e5770323667cf1830e6ba69d90
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-09-19 09:54:10 +0100

    made changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,38 +1,150 @@
+import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
 import { images } from "../constants";
+import toast from "react-hot-toast";
 
 const Hero = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState(null);
+  const [success, setSuccess] = useState("");
+  const [existingEmails, setExistingEmails] = useState([]);
+  const sheetdbapi = import.meta.env.VITE_SHEET_DB;
+
+  const handleOpenModal = () => {
+    window.document.body.classList.add("overflow-hidden");
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    window.document.body.classList.remove("overflow-hidden");
+
+    setIsModalOpen(false);
+    setSubmitError(null);
+  };
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        const response = await fetch(
+          `https://sheetdb.io/api/v1/${sheetdbapi}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        const emails = data.map((item) => item.email).filter(Boolean);
+        setExistingEmails(emails);
+        // console.log("Fetched emails:", emails);
+      } catch (error) {
+        console.error("Error fetching emails:", error);
+      }
+    };
+    handleFetch();
+  }, [sheetdbapi]);
+
+  // console.log(existingEmails);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setSubmitError(null);
+
+    const formData = new FormData(e.target);
+    const name = formData.get("fullName");
+    const email = formData.get("email");
+
+    if (existingEmails.includes(email)) {
+      setSubmitError("Email is already on wait list");
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(`https://sheetdb.io/api/v1/${sheetdbapi}`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              name: name,
+              email: email,
+            },
+          ],
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // const data = await response.json();
+      // console.log(data);
+      handleCloseModal();
+      // You might want to show a success message here
+      setSuccess("You have been added to wait list successfully");
+      toast.success("Thanks for joining our wait list we'll keep you updated");
+    } catch (error) {
+      console.error("Error:", error);
+      setSubmitError("An error occurred. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <section className="min-h-screen bg-linen pt-28 max-sm:pt-16">
-      <div className="container mx-auto sm:pl-4 flex flex-col sm:flex-row items-center justify-between gap-8 sm:gap-12 lg:gap-16">
-        <div className="flex flex-col gap-4 sm:gap-6 px-3 lg:gap-8 w-full sm:w-1/2">
-          <h1 className="text-2xl sm:text-3xl lg:text-5xl font-bold">
+    <section className="min-h-screen bg-linen pt-28 max-sm:pt-16 flex pl-4 max-md:flex-col items-center justify-between max-md:gap-y-9">
+      <div className="flex flex-col gap-x-4 px-3 w-full">
+        <div className="space-y-2">
+          <h1 className="max-sm:text-3xl max-2xl:text-4xl font-bold font-MerriweatherSans">
             Corem ipsum dolor sit amet, consectetur.
           </h1>
-          <div className="space-y-2">
-            <p className="text-base sm:text-lg lg:text-xl font-normal">
-              Corem ipsum dolor sit amet, consectetur.
-            </p>
-            <p className="text-base sm:text-lg lg:text-xl font-normal">
-              Corem ipsum dolor sit amet, consectetur.
-            </p>
-          </div>
-          <button className="self-start mt-2 sm:mt-4 h-11 max-sm:w-full text-center border border-primary transition-all duration-500 hover:bg-whitesmoke rounded-lg text-primary text-base max-sm:text-center font-montserrat font-normal items-center py-3 px-7 ">
-            Join Our Community
+          <p className="text-xl font-normal font-montserrat">
+            Corem ipsum dolor sit amet, consectetur. Corem ipsum dolor sit amet,
+            consectetur.
+          </p>
+        </div>
+        {success ? (
+          <p className="text-base mt-2 italic text-green-600">{success}</p>
+        ) : (
+          <button
+            onClick={handleOpenModal}
+            className="self-start mt-2 sm:mt-4 h-11 max-sm:w-full text-center border border-primary transition-all duration-500 hover:bg-whitesmoke rounded-lg text-primary text-base max-sm:text-center font-montserrat font-normal items-center py-3 px-7 "
+          >
+            Join waitlist here
           </button>
-        </div>
-        <div className="w-full sm:w-1/2">
-          <img
-            className="hidden sm:block w-full h-auto object-cover"
-            src={images.heroDesktop}
-            alt="hero"
-          />
-          <img
-            className="sm:hidden w-full h-auto object-cover"
-            src={images.heroMobile}
-            alt="hero"
-          />
-        </div>
+        )}
       </div>
+
+      <div className="w-full ">
+        <img
+          className="max-md:hidden w-full h-auto object-cover"
+          src={images.heroDesktop}
+          alt="hero"
+        />
+        <img
+          className="max-md:block hidden w-full h-auto object-cover"
+          src={images.heroMobile}
+          alt="hero"
+        />
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSubmit={handleSubmit}
+        isSubmitting={isSubmitting}
+        submitError={submitError}
+      />
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,15 +19,29 @@ export default {
         MerriweatherSans: ["Merriweather Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
-    },
-    colors: {
-      primary: "#0F0140",
-      darkslateBlue: "#1D143E",
-      black: "#262526",
-      linen: "#F2EDE4",
-      yellow: "#EAF206",
-      whitesmoke: "#F9F6F3",
-      white: "#FFFFFF",
+      colors: {
+        primary: "#0F0140",
+        darkslateBlue: "#1D143E",
+        black: "#262526",
+        linen: "#F2EDE4",
+        yellow: "#EAF206",
+        whitesmoke: "#F9F6F3",
+        white: "#FFFFFF",
+      },
+      animation: {
+        "slide-in-right": "slide-in-right 0.4s ease-in-out ",
+      },
+      keyframes: {
+        "slide-in-right": {
+          "0%": {
+            visibility: "visible",
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
     },
   },
   plugins: [],

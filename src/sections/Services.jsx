5bba85a2a2a0a5e9b9b05f21bb06432a848f8108
Smi-lin <olajideolabimpe26@gmail.com>
2024-09-20 15:34:47 +0100

    Updated

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
       <div className=" mx-auto px-4 2xl:px-0 max-w-[1600px]">
         <div className="flex flex-col md:flex-row justify-between items-center md:items-start">
           {/* Image Section */}
-          <div className="md:w-2/5 md:mt-8 md:mr-8 max-md:order-last">
+          <div className="md:w-2/5 mt-8 md:mr-8 max-md:order-last max-md:mt-10">
             <img
               src={images.balanceDesktop}
               alt="Balance Desktop"
@@ -14,14 +14,14 @@ const Services = () => {
             />
           </div>
           {/* Text Section */}
-          <div className="md:w-3/5 text-center max-sm:text-left md:pl-8 xl:pl-16 2xl:pl-24 pt-8 md:pt-24 ">
+          <div className="md:w-3/5 text-left md:pl-8 xl:pl-16 2xl:pl-24 pt-8 md:pt-24  ">
             <h2 className="max-sm:text-3xl text-4xl text-[#0F0140] mb-4 font-MerriweatherSans">
               Corem ipsum dolor sit amet, consectetur.
             </h2>
-            <p className="text-sm sm:text-xl md:text-sm xl:text-sm mb-2 leading-4 font-montserrat text-primary">
+            <p className="text-sm sm:text-xl md:text-sm xl:text-sm mb-2  font-montserrat text-primary">
               Corem ipsum dolor sit amet, consectetur.
             </p>
-            <p className="text-sm sm:text-xl md:text-sm xl:text-sm font-montserrat text-primary max-sm:mb-14">
+            <p className="text-sm sm:text-xl md:text-sm xl:text-sm font-montserrat text-primary">
               Corem ipsum dolor
             </p>
           </div>
